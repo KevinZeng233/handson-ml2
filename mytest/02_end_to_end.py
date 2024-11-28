0d655c98efab8a534cc866b1d359304525725c7a
KevinZeng233 <zerohub123@outlook.com>
2024-11-28 16:51:49 +0800

    整理end_to_end

---
+++ mytest/02_end_to_end.py
@@ -0,0 +1,48 @@
+# Python ≥3.5 is required
+import sys
+assert sys.version_info >= (3, 5)
+
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# Common imports
+import numpy as np
+import os
+import pandas as pd
+from pathlib import Path
+
+# 当前脚本文件路径
+current_file_path = Path(__file__)
+# 上一级文件夹路径
+parent_folder_path = current_file_path.parent.parent
+# "datasets" 文件夹路径
+HOUSING_PATH = parent_folder_path / "datasets" / "housing"
+# HOUSING_PATH = r"E:/github/handson-ml2/datasets/housing"
+
+# To plot pretty figures
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "end_to_end_project"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+housing = load_housing_data()
+housing.info()
